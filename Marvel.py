51c35d3fb3bb88ca59afc3fcc5cde6a4bf63c3b0
Kenrick Halff <kenrickhalff@gmail.com>
2018-10-16 22:13:15 +0200

    Renamed all variables to English. Generating 4 random options with 1 correct answer, these options are stored in a list of dicts. The dicts consist of name and a Bool called correct. Upped The API results limit to 100.

--- Marvel.py
+++ Marvel.py
@@ -1,8 +1,12 @@
 import time
 import hashlib
 import json
+import random
+import requests
+import math
 
-def alleCharacters():
+
+def getAllCharactersFromAPI():
     #Maken van de API Url
 
     timestamp = time.time()
@@ -11,40 +15,96 @@ def alleCharacters():
     stringToHash = str(timestamp) + privateApiKey + publicApiKey
     hash_object = hashlib.md5(stringToHash.encode())
     md5Hash = hash_object.hexdigest()
-    apiUrl = 'http://gateway.marvel.com/v1/public/characters?ts={}&apikey={}&hash={}'.format(timestamp, publicApiKey, md5Hash)
-    print(apiUrl)   # deze moet op gegeven moment uit de code weggehaald worden
+    apiUrl = 'http://gateway.marvel.com/v1/public/characters?ts={}&apikey={}&hash={}&limit={}'.format(timestamp, publicApiKey, md5Hash, 100)
+
     # voorbeeld voor hoe het eruit komt te zien en deze kun je dan in Visual Studio Code gebruiken
     # http://gateway.marvel.com/v1/public/characters?ts=1539531767.861436&apikey=ce72ea27bb97e27dbf4b8be2decb44ee&hash=1efc26ce5b2905641dd9413bfe6662d8
 
 
     # Aanroepen van API Url
 
-    import requests #pip install requests uitvoeren in Terminal om te installeren voor de 1e keer
+    #import requests #pip install requests uitvoeren in Terminal om te installeren voor de 1e keer
     response = requests.get(apiUrl)
 
     # Resultaten van API in Json omzetten naar dictionary
-    apiResultaten = json.loads(response.text)
+    apiResults = json.loads(response.text)
 
     # Alle characters uitlezen uit API
-    allCharacters = apiResultaten['data']['results']
+    allCharacters = apiResults['data']['results']
 
-    return  allCharacters
+    return allCharacters
 
-def kiesCharacter(allCharacters):
+
+def chooseRandomCharacter(allCharacters, excludedCharacter={"name": ""}):
     # Code aanmaken om een character random te kiezen
-    return allCharacters[0]
 
-def geefHint(gekozenCharacter):
+    randomNumber = random.randrange(0, len(allCharacters))
+    print(randomNumber, len(allCharacters))
+    chosenCharacter = allCharacters[randomNumber]
+    while chosenCharacter["description"] == "" and chosenCharacter["name"] != excludedCharacter["name"]:
+        randomNumber = random.randrange(len(allCharacters))
+        chosenCharacter = allCharacters[randomNumber]
+
+    return chosenCharacter
+
+
+def generateHint(chosenCharacter):
     # Code aanmaken om hint te genereren
-    return "tekst"
+
+    description = str(chosenCharacter["description"])
+    hint = description.replace(chosenCharacter["name"], "[...]")
+    return hint
+
+
+def checkAnswer(answer, options):
+    if options[answer]["correct"]:
+        return True
+    else:
+        return False
+
+
+def generateOptions(correctCharacter, allCharacters):
+    options = [
+        {
+            "name": correctCharacter["name"],
+            "correct": True
+        }
+    ]
+
+    for i in range(0, 3):
+        options.append({
+            "name": chooseRandomCharacter(allCharacters)["name"],
+            "correct": False
+        })
+
+    return options
+
 
 # start programma
 print('quizgestart')
-naam = input('Wat is je naam: ')
-allCharacters = alleCharacters() #Finish
-gekozenCharacter = kiesCharacter(allCharacters)#nog open #LET OP: Er mag geen Character gekozen worden met een lege discription, daar kunnen we geen hints vandaan halen
-
-hint = geefHint(gekozenCharacter)# nog open
+name = input('Wat is je naam: ')
+allCharacters = getAllCharactersFromAPI() #Finish
+chosenCharacter = chooseRandomCharacter(allCharacters)#nog open #LET OP: Er mag geen Character gekozen worden met een lege discription, daar kunnen we geen hints vandaan halen
+print(chosenCharacter)
+hint = generateHint(chosenCharacter)# nog open
+print(hint)
 # hints eerst uit discription halen van de gekozen character
 
+options = generateOptions(chosenCharacter, allCharacters)
 
+print(options)
+
+userInput = input("Druk '1', '2', '3' of '4' voor uw antwoord of voer 'help' in voor een hint")
+correct = False
+
+if userInput == 'help':
+    print(hint)
+elif userInput.isnumeric():
+    correct = checkAnswer(int(userInput) - 1, options)
+else:
+    print("Ongeldige input, programma word afgesloten")
+
+if correct:
+    print("U heeft het spel gewonnen!")
+else:
+    print("U heeft het foute antwoord")
