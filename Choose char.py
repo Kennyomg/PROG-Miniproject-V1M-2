b4b8b62063f6d46285ebdac042ed4318ee25c7e1
karmalegend <43471135+karmalegend@users.noreply.github.com>
2018-10-17 17:38:20 +0200

    Choose char function

---
+++ Choose char.py
@@ -0,0 +1,25 @@
+def chooseCharacter(apilst):
+    allCharacters = apilst #read api list
+    charlst = [] #create fresh list for char names
+    descriplst = [] #create fresh list for descriptions
+
+    for character in allCharacters:
+        charlst.append(character['name']) #add char to char list
+        descriplst.append(character['description']) #add descrip to descrip list
+
+    randomselect = random.choice(charlst) #select random char
+
+    while True:
+        descriptionchar = descriplst[charlst.index(randomselect)]
+        if len(descriptionchar) >= 1: #if descrip of selected char >1 let prog know
+            print('{}'.format(randomselect))
+            break
+        else:                                                 #if descrip of selected char non exist let prog know
+            randomselect = random.choice(charlst)
+
+
+    return{'character':randomselect,'description':descriptionchar} #return character and descrip for later use
+
+
+
+chooseCharacter(MarvelCharacters())
